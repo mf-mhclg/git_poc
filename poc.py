416c6bc2c3a91a63e12d9a5ee867ed8c69c4d22d
Mark Foster <mark.foster@communities.gov.uk>
2022-02-04 14:54:50 +0000

    test

---
+++ poc.py
@@ -0,0 +1,11 @@
+
+
+def hello_world():
+    
+    hi = "hello"
+
+    return hi
+
+
+hi=hello_world()
+print(hi)
---
+++ test.py
@@ -0,0 +1,8 @@
+import pytest
+# import poc
+
+def test_hello_world():
+    # x = poc.hello_world
+    # assert x==x
+    assert 'sdsad'=='wrong'
+
